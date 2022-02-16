6d07c58451619264ba6d941cdb59340a48697352
Ailany <88301756+ailanyEXE@users.noreply.github.com>
2022-02-16 14:41:23 -0500

    initial commit

---
+++ src/portfolio-container/home/Profile.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Profile(){
+
+    return(
+
+        <div className='profile-container'>
+            <div className='profile-parent'> 
+                <div className='profile-details'>
+                    <div className='cols'>
+                        <h1>Bloop.</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    );
+
+
+}
